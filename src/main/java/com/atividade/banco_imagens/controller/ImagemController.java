ad159ef8b36a900963b8c353ab287914e81bf3f7
Lívia Neves Rodrigues Vitor Estevam <neveslivia95@gmail.com>
2025-06-06 17:36:23 -0300

    feat: adicionando a classe config para conseguir exibir magens externas via proxy

--- src/main/java/com/atividade/banco_imagens/controller/ImagemController.java
+++ src/main/java/com/atividade/banco_imagens/controller/ImagemController.java
@@ -1,5 +1,7 @@
 package com.atividade.banco_imagens.controller;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.atividade.banco_imagens.model.ImagemModel;
@@ -15,6 +18,11 @@ import com.atividade.banco_imagens.service.ImagemService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.net.URL;
+
 
 @RestController
 @CrossOrigin(origins = "*") 
@@ -24,13 +32,29 @@ public class ImagemController {
    @Autowired
     ImagemService imagemService;
 
-
-
-    
     @PostMapping
     public ImagemModel salvar(@RequestBody ImagemModel imagemModel) {        
         return imagemService.salvarImagem(imagemModel);
     }
+
+    @GetMapping("/proxy")
+    public void proxyImagem(@RequestParam("url") String url, HttpServletResponse response) {
+    try {
+        URL imageUrl = new URL(url);
+        HttpURLConnection conn = (HttpURLConnection) imageUrl.openConnection();
+        conn.setRequestProperty("User-Agent", "Mozilla/5.0");
+        conn.connect();
+
+        response.setContentType(conn.getContentType());
+        try (InputStream is = conn.getInputStream()) {
+            is.transferTo(response.getOutputStream());
+        }
+        response.flushBuffer();
+    } catch (Exception e) {
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+    }
+}
+
 
     @GetMapping
     public List<ImagemModel> listar(){
@@ -53,9 +77,5 @@ public class ImagemController {
     public void deletar(@PathVariable Long id){
         imagemService.deletarImagem(id);
     }
-
-    
-    
-    
 
 }
